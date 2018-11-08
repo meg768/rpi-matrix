20b110284fd956d51abfe13d2a7ac12e3c9ef0ca
meg768 <meg768@users.noreply.github.com>
2018-11-08 20:57:00 +0100

    -

--- tests/foo.js
+++ tests/foo.js
@@ -7,7 +7,7 @@ var size = 32 * 32;
 function RGB(red, green, blue) {
     return (red << 16) | (green << 8) | blue;;
 }
-
+/*
 var pixels  = new Uint32Array(size);
 
 for (var x = 0; x < 32; x++) {
@@ -15,8 +15,7 @@ for (var x = 0; x < 32; x++) {
         pixels[y * 32 + x] = RGB(128, 0, 0);
     }
 }
-
-/*
+*/
 
 var pixels  = new Buffer(size * 4);
 var offset = 0;
@@ -28,7 +27,7 @@ for (var x = 0; x < 32; x++) {
         pixels[offset++] = 0;
     }
 }
-*/
+
 matrix.draw(pixels);
 matrix.update();
 
