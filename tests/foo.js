157d2dc35eab9e5256589ba48a83c2e5d212b372
meg768 <meg768@users.noreply.github.com>
2018-11-08 22:25:39 +0100

    -

--- tests/foo.js
+++ tests/foo.js
@@ -1,38 +1,44 @@
 
 var Matrix = require('../index.js');
 
-var matrix = new Matrix({width:32, height:32});
-var size = 32 * 32;
 
-function RGB(red, green, blue) {
-    return (red << 16) | (green << 8) | blue;;
-}
+class Canvas {
+    constructor() {
+        this.matrix = new Matrix({width:32, height:32});
+        this.width = 32;
+        this.height = 32;
+        this.pixels = new Uint32Array(this.width * this.height);
+    }
 
+    static RGB(red, green, blue) {
+        return (red << 16) | (green << 8) | blue;;
+    }
 
-var pixels  = new Uint32Array(size);
+    fill(color) {
+        for (var x = 0; x < this.width; x++) {
+            for (var y = 0; y < this.height; y++) {
+                this.pixels[y * 32 + x] = this.RGB(0, 0, 128);
+            }
+        }
+    }
 
-for (var x = 0; x < 32; x++) {
-    for (var y = 0; y < 32; y++) {
-        pixels[y * 32 + x] = RGB(0, 0, 128);
+    render() {
+
     }
 }
 
-/*
-var pixels  = new Buffer(size * 4);
-var offset = 0;
-for (var x = 0; x < 32; x++) {
-    for (var y = 0; y < 32; y++) {
-        pixels[offset++] = 0;
-        pixels[offset++] = 128;
-        pixels[offset++] = 0;
-        pixels[offset++] = 0;
-    }
+
+function main() {
+
+    var canvas = new Canvas();
+    canvas.fill(canvas.RGB(128, 0, 0));
+
+    setTimeout(function(){ console.log("Hello"); }, 3000);
+
 }
-*/
-matrix.draw(pixels);
-matrix.update();
+
+main();
 
 
 
-setTimeout(function(){ console.log("Hello"); }, 3000);
 
