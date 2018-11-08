4e11bba69d1492688a0f41124d27dc89d5faae4a
meg768 <meg768@users.noreply.github.com>
2018-11-08 18:28:23 +0100

    -

--- tests/foo.js
+++ tests/foo.js
@@ -3,20 +3,19 @@ var Matrix = require('../index.js');
 
 var matrix = new Matrix({width:32, height:32});
 var size = 32 * 32;
-var buffer = new Buffer(size*8);
-
+var buffer = new Buffer(size*4);
+var offset = 0;
 for (var x = 0; x < 32; x++) {
     for (var y = 0; y < 32; y++) {
-        var offset = y * 32 + x;
-        buffer[offset+0] = 128;
-        buffer[offset+1] = 128;
-        buffer[offset+2] = 128;
-        buffer[offset+3] = 128;
+        buffer[offset++] = 128;
+        buffer[offset++] = 128;
+        buffer[offset++] = 128;
+        buffer[offset++] = 128;
     }
 }
 
 
-matrix.draw(buffer.buffer);
+matrix.draw(buffer);
 matrix.update();
 
 
