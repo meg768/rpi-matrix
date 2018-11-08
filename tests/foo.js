dda8da5a1816a2e93e41cfd17b8a55d7941edbbf
meg768 <meg768@users.noreply.github.com>
2018-11-08 20:38:41 +0100

    -

--- tests/foo.js
+++ tests/foo.js
@@ -3,19 +3,22 @@ var Matrix = require('../index.js');
 
 var matrix = new Matrix({width:32, height:32});
 var size = 32 * 32;
-//var pixels  = new Uint32Array(size);
-var pixels  = new Buffer(size * 4);
 
 function RGB(red, green, blue) {
     return (red << 16) | (green << 8) | blue;;
 }
-/*
+
+var pixels  = new Uint32Array(size);
+
 for (var x = 0; x < 32; x++) {
     for (var y = 0; y < 32; y++) {
         pixels[y * 32 + x] = RGB(128, 0, 0);
     }
 }
-*/
+
+/*
+
+var pixels  = new Buffer(size * 4);
 var offset = 0;
 for (var x = 0; x < 32; x++) {
     for (var y = 0; y < 32; y++) {
@@ -25,7 +28,7 @@ for (var x = 0; x < 32; x++) {
         pixels[offset++] = 0;
     }
 }
-
+*/
 matrix.draw(pixels);
 matrix.update();
 
