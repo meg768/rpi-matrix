1dbb28b7be89a61406451421768b9b6f03906528
meg768 <magnus@egelberg.se>
2018-11-11 23:20:23 +0100

    -

--- examples/scripts/emoji-text.js
+++ examples/scripts/emoji-text.js
@@ -4,20 +4,21 @@ class Sample  {
 
 
     parse(text) {
-        var regexp = new RegExp(':[^\s]*:');
+        var regexp = new RegExp(':*:');
 
-        //console.log(regexp.split(text));
+        var match = regexp.split(text);
+        console.log(match);
 
-        var split = text.split(/:*:/);
+        //var split = text.split(/:*:/);
 
-        console.log('Text', text);
-        split.forEach((element, index) => {
-            console.log(index, element);
+        //console.log('Text', text);
+        //split.forEach((element, index) => {
+          //  console.log(index, element);
             
-        });
+        //});
     }
     run() {
-        this.parse('Jag vill ha en ::beer::. Eller ::ice-gream:: nu. Eller kanske ::kalle olle::?!');
+        this.parse('Jag vill ha en :beer:. Eller en :hotdog: nu. Eller kanske ::kalle olle::?!');
     }
 };
 
