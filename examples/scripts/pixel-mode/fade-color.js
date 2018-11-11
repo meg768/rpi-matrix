04ac4f9778f4305c92d1ced3669ea4b98b807c84
meg768 <magnus@egelberg.se>
2018-11-12 00:01:00 +0100

    -

--- examples/scripts/pixel-mode/fade-color.js
+++ examples/scripts/pixel-mode/fade-color.js
@@ -4,14 +4,22 @@ var Matrix = require('../../../index.js');
 class Sample extends Matrix {
 
     run() {
-        // Fill matrix with red and blend in (from black) in 100 steps
+        // Fill matrix with red and blend in (from black) using 500 steps
         this.fill(this.RGB(255, 0, 0));
-        this.render({blend:100});
+        this.render({blend:500});
 
-        // Render contents again, but with a pause after rendering
+        // Fill matrix with green and blend in using 500 steps
+        this.fill(this.RGB(0, 255, 0));
+        this.render({blend:500});
+
+        // Fill matrix with blue and blend in using 500 steps
+        this.fill(this.RGB(0, 0, 255));
+        this.render({blend:500});
+        
+        // Render contents again, but with a pause
         this.render({sleep:1000});
 
-        // Fade out to black, again using 100 steps
+        // Fade out to black...
         this.fill(this.RGB(0, 0, 0));
         this.render({blend:100});
 
--- examples/scripts/pixel-mode/glow-red.js
+++ examples/scripts/pixel-mode/glow-red.js
@@ -4,17 +4,20 @@ var Matrix = require('../../../index.js');
 class Sample extends Matrix {
 
     constructor(options) {
-        super(options);
+        super({...options, ...{mode:'pixel'}});
     }
 
     run() {
         var red;
 
+        // Loop from black to red and render each color
         for (red = 0; red <= 255; red++) {
             this.fill(this.RGB(red, 0, 0));
             this.render();
         
         }
+
+        // Loop back to black
         for (red = 255; red >= 0; red--) {
             this.fill(this.RGB(red, 0, 0));
             this.render();
@@ -23,5 +26,5 @@ class Sample extends Matrix {
     }
 }
 
-var sample = new Sample({mode:'pixel', width:32, height:32});
+var sample = new Sample({width:32, height:32});
 sample.run();
