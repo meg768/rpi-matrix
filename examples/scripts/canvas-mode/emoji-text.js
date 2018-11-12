953eee3005b71b166699d0832af439f4afeb8208
meg768 <meg768@users.noreply.github.com>
2018-11-12 15:47:44 +0100

    -

--- examples/scripts/canvas-mode/emoji-text.js
+++ examples/scripts/canvas-mode/emoji-text.js
@@ -1,26 +1,58 @@
 #!/usr/bin/env node
 
+
+class EmojiParser  {
+
+    constructor(emojis) {
+        this.emojis = emojis;
+    }
+
+    exists(name) {
+        var result = this.emojis.find((emoji) => {
+            return emoji == name;
+        });
+
+        return result != undefined;
+    }
+
+    parse(text) {
+
+        var output = [];
+        var regexp = new RegExp(/(\:[\w\-\+]+\:)/g);
+
+        text.split(regexp).forEach((text) => {
+
+            if (text.match(regexp)) {
+                var emoji = text.replace(/:/g, '');
+
+                if (this.exists(emoji))
+                    output.push({emoji:emoji});
+                else
+                    output.push({text:text});
+            }
+            else {
+                output.push({text:text});
+            }
+        });
+
+        console.log(output);
+        return output;
+    }
+
+
+};
+
+
 class Sample  {
 
 
-    parse(text) {
-        var regexp = new RegExp(':*:');
 
-        var match = regexp.split(text);
-        console.log(match);
-
-        //var split = text.split(/:*:/);
-
-        //console.log('Text', text);
-        //split.forEach((element, index) => {
-          //  console.log(index, element);
-            
-        //});
-    }
     run() {
-        this.parse('Jag vill ha en :beer:. Eller en :hotdog: nu. Eller kanske ::kalle olle::?!');
+        var parser = new EmojiParser(['beer', 'ice_cream', 'joy', 'fries']);
+        parser.parse('Jag vill ha en :beer:. Eller :ice_cream: :foo: nu. Eller kanske :kalle olle:?!');
     }
-};
+}
+
 
 var sample = new Sample({mode:'canvas', width:32, height:32});
 sample.run();
