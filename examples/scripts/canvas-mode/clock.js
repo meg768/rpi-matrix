e64fdc365f4c94237c4e6f27ca1c28788ce4fef3
meg768 <magnus@egelberg.se>
2018-11-19 00:38:44 +0100

    -

--- examples/scripts/canvas-mode/clock.js
+++ examples/scripts/canvas-mode/clock.js
@@ -10,10 +10,22 @@ var fs = require('fs');
 class Sample extends Matrix {
 
 
-    getLayerImage(psd, name) {
+    constructor(options) {
+        super(options);
+
+
+    }
+
+    getLayer(psd, name) {
+        var psdWidth = psd.header.rows; 
+        var psdHeight = psd.header.cols; 
+
         return new Promise((resolve, reject) => {
             var layer = psd.tree().childrenAtPath(name)[0];
             var image = layer.get('image');
+
+            var canvas = this.createCanvas(psdWidth, psdHeight);
+            var ctx = canvas.getContext('2d');
     
             image.saveAsPng('X.png').then(() => {
                 return Promise.resolve();
@@ -22,54 +34,103 @@ class Sample extends Matrix {
                 return this.loadImage('X.png');
             })
             .then((image) => {
-                resolve(image);
+                ctx.drawImage(image, layer.left, layer.top, layer.width, layer.height);
+                resolve(canvas);
             })
             .catch(error => {
                 reject(error);
             });
     
         });
-
     }
+
+    
+    getLayers(psd) {
+        return new Promise((resolve, reject) => {
+
+            var layersNames = ['background', 'hours', 'minutes', 'seconds'];
+            var promise = Promise.resolve();
+            var layers = {};
+
+            layersNames.forEach((layerName) => {
+                promise = promise.then(() => {
+                    return this.getLayer(psd, layerName);
+                })
+                .then((canvas) => {
+                    layers[layerName] = canvas;
+                });
+            });
+
+            promise.then(() => {
+                resolve(layers);
+            })
+            .catch(error => {
+                reject(error);
+            })
+        });
+    }
+
+
 
     run() {
         var fileName = path.join(__dirname, '../../clocks', 'swiss-red' + '.psd');
+
+
+        console.log('Loading PSD...');
         var psd = PSD.fromFile(fileName);
-
-
         psd.parse();
+        
 
-        this.getLayerImage(psd, 'background').then((image) => {
-            var ctx = this.canvas.getContext('2d');
-            ctx.drawImage(image, 0, 0, this.width, this.height);
-            this.render();
-            this.sleep(3000);
+        var psdWidth = psd.header.rows; 
+        var psdHeight = psd.header.cols; 
+
+        console.log(psdWidth, psdHeight);
+
+        this.getLayers(psd).then((layers) => {
+            for (;;) {
+
+                var date = new Date();
+
+                var hour   = (date.getHours() + date.getMinutes() / 60) / 12;
+                var minute = date.getMinutes() / 60; 
+                var second = date.getSeconds() / 60;
+    
+                var canvas = this.createCanvas(psdWidth, psdHeight);
+                var ctx = canvas.getContext('2d');
+    
+    
+                ctx.drawImage(layers['background'], 0, 0, canvas.width, canvas.height);
+    
+                ctx.save();
+                ctx.translate(canvas.width/2, canvas.height/2);
+                ctx.rotate(hour * 2 * Math.PI);
+                ctx.drawImage(layers['hours'], -canvas.width/2, -canvas.height/2);
+                ctx.restore();
+    
+                ctx.save();
+                ctx.translate(canvas.width/2, canvas.height/2);
+                ctx.rotate(minute * 2 * Math.PI);
+                ctx.drawImage(layers['minutes'], -canvas.width/2, -canvas.height/2);
+                ctx.restore();
+    
+                ctx.save();
+                ctx.translate(canvas.width/2, canvas.height/2);
+                ctx.rotate(second * 2 * Math.PI);
+                ctx.drawImage(layers['seconds'], -canvas.width/2, -canvas.height/2);
+                ctx.restore();
+    
+                this.canvas.getContext('2d').clearRect(0, 0, this.width, this.height);
+                this.canvas.getContext('2d').drawImage(canvas, 0, 0, this.width, this.height);
+
+                this.render();
+                this.sleep(0);
+    
+            }
     
         })
         .catch(error => {
             console.log(error);
         })
-/*
-        var background = psd.tree().childrenAtPath('background')[0];
-        var image = background.get('image');
-        var png = image.toPng();
-
-        var stream = this.canvas.createPNGStream();
-        console.log(png instanceof Buffer);
-        console.log(png instanceof Object);
-        console.log(png);
-        image.saveAsPng('X.png');
-
-        
-        var ctx = this.canvas.getContext('2d');
-        ctx.drawImage(png, 0, 0, this.width, this.height);
-        this.render();
-        this.sleep(1000);
-*/
-//        console.log(background.get('image'));
-//        background.toPng();
-  //      console.log(background.export());
-//        console.log(psd.tree().export());
         
     }
 };
