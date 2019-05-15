86a476a80c09cc422be64767f2c9ef6e2096ce60
meg768 <meg768@users.noreply.github.com>
2019-05-15 17:53:04 +0200

    -

--- text-animation.js
+++ text-animation.js
@@ -4,7 +4,7 @@ var path = require('path');
 var Animation = require('./demo/src/scripts/animation.js');
 
 
-class TextAnimation extends Animation  {
+module.exports = class TextAnimation extends Animation  {
 
     constructor(options) {
 
@@ -231,64 +231,6 @@ class TextAnimation extends Animation  {
     }    
 
 };
-
-
-
-
-class Command {
-
-    constructor() {
-        module.exports.command  = 'text [options]';
-        module.exports.describe = 'Scroll text';
-        module.exports.builder  = this.defineArgs;
-        module.exports.handler  = this.run;
-        
-    }
-
-    defineArgs(args) {
-
-        args.usage('Usage: $0 [options]');
-
-        args.option('help', {describe:'Displays this information'});
-        args.option('text', {describe:'Text to display', default:'Hello World'});
-        args.option('textColor', {describe:'Specifies text color', default:'blue'});
-
-        args.wrap(null);
-
-        args.check(function(argv) {
-            return true;
-        });
-
-        return args.argv;
-    }
-
-
-    run(argv) {
-
-        try {
-
-            Matrix.configure(argv);
-
-            var animation = new TextAnimation(argv);
-
-            Promise.resolve().then(() => {
-                return animation.run();
-            })
-            .catch(error => {
-                console.error(error.stack);
-            })
-        }
-        catch (error) {
-            console.error(error.stack);
-        }
-
-    }
-    
-
-
-};
-
-new Command();
 
 
 
