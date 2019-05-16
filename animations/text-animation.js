93445b96372e3d5cc18a5d771c822fd42ea44ca9
meg768 <meg768@users.noreply.github.com>
2019-05-16 20:46:53 +0200

    -

--- animations/text-animation.js
+++ animations/text-animation.js
@@ -1,7 +1,7 @@
 var root = '..';
 var path = require('path');
 
-var Matrix = require(path.join(root, './matrix.js');
+var Matrix = require(path.join(root, './matrix.js'));
 var Animation = require(path.join(root, './src/js/animation.js'));
 var ScrollAnimation = require(path.join(root, './animations/scroll-animation.js'));
 
