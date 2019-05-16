2412671d726d98b6c404752731e729bb24c84758
meg768 <meg768@users.noreply.github.com>
2019-05-16 16:59:44 +0200

    -

--- demo/demo.js
+++ demo/demo.js
@@ -36,7 +36,7 @@ var App = function() {
             args.option('led-cols',         {describe:'Number of columns for display', default:64});
             args.option('led-rows',         {describe:'Number of rows for display', default:32});
             args.option('led-gpio-mapping', {describe:'Type of hardware used', default:'regular'});
-            args.option('led-rgb-sequence', {describe:'Matrix RGB color order', default:'RBG'});
+            args.option('led-rgb-sequence', {describe:'Matrix RGB color order', default:'RGB'});
             args.option('led-scan-mode',    {describe:'Scan mode (0/1)', default:0});
 
 			loadCommands(args);
