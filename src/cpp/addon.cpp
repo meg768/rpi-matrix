393be0b27085a77b2b9bb42f96556e3c35b09746
meg768 <meg768@users.noreply.github.com>
2019-05-17 22:12:43 +0200

    -

--- src/cpp/addon.cpp
+++ src/cpp/addon.cpp
@@ -73,7 +73,7 @@ NAN_METHOD(Addon::configure)
         rows = options->Get(Nan::New<v8::String>("led_rows").ToLocalChecked());
 
     if (!rows->IsUndefined())
-        opts.rows = ToInt32(Nan::GetCurrentContext()).ToLocalChecked()->Value();
+        opts.rows = rows->ToInt32(Nan::GetCurrentContext()).ToLocalChecked()->Value();
 
     // cols
     v8::Local<v8::Value> cols = options->Get(Nan::New<v8::String>("led-cols").ToLocalChecked());
