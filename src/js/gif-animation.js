acbfe90f2e23e01e8e78583c88d30fce949f269f
meg768 <meg768@users.noreply.github.com>
2019-05-17 11:50:08 +0200

    -

--- src/js/gif-animation.js
+++ src/js/gif-animation.js
@@ -16,7 +16,7 @@ module.exports = class GifAnimation extends Animation {
         this.gif = gif;
     }
 
-/*
+
     stop() {
         return new Promise((resolve, reject) => {
 
@@ -42,24 +42,14 @@ module.exports = class GifAnimation extends Animation {
 
                 var GIF = require('omggif');
                 var gif = new GIF.GifReader(this.loadGIF(this.gif));
-                var ctx = this.matrix.canvas.getContext('2d');
                 var numFrames = gif.numFrames();
         
                 var canvas = this.matrix.createCanvas(gif.width, gif.height);
         
-                var scaleX = this.matrix.width  / gif.width;
-                var scaleY = this.matrix.height / gif.height;
-        
-                ctx.scale(scaleX, scaleY);
-
-                if (scaleX > 1 || scaleY > 1)
-                    ctx.imageSmoothingEnabled = false;                
-
                 this.context = {};
                 this.context.gif = gif;
                 this.context.numFrames = numFrames;
                 this.context.canvas = canvas;
-                this.context.ctx = ctx;
                 this.context.currentFrame = 0;
             })
             .then(() => {
@@ -75,18 +65,24 @@ module.exports = class GifAnimation extends Animation {
     render() {
         var context = this.context;
 
-        var frame = context.gif.frameInfo(i);
-        var image = context.ctx.createImageData(gif.width, gif.height);
+        var ctx = context.canvas.getContext("2d");
+
+        var frame = context.gif.frameInfo(context.currentFrame);
+        var image = ctx.createImageData(gif.width, gif.height);
         context.gif.decodeAndBlitFrameRGBA(context.currentFrame, image.data);
 
-        canvas.getContext("2d").putImageData(image, 0, 0);
-        ctx.drawImage(canvas, 0, 0);
+        ctx.putImageData(image, 0, 0);
+        this.matrix.canvas.getContext("2d").drawImage(canvas, 0, 0);
+        context.currentFrame++;
 
-        this.matrix.render();
-        this.matrix.sleep(frame.delay * 10);
+        if (context.currentFrame > context.numFrames)
+            context.currentFrame = 0;
+
+            //this.matrix.render();
+        //this.matrix.sleep(frame.delay * 10);
 
     }
-*/
+
     loadGIF(name) {
 
         function fileExists(path) {
@@ -114,7 +110,7 @@ module.exports = class GifAnimation extends Animation {
     }
 
 
-    run() {
+    runX() {
         var GIF = require('omggif');
         var gif = new GIF.GifReader(this.loadGIF(this.gif));
         var ctx = this.matrix.canvas.getContext('2d');
