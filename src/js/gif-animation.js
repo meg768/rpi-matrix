505169afd7909b692be9a8ef9cd4f952fe9cb7fb
meg768 <meg768@users.noreply.github.com>
2019-05-17 11:28:46 +0200

    -

--- src/js/gif-animation.js
+++ src/js/gif-animation.js
@@ -16,11 +16,12 @@ module.exports = class GifAnimation extends Animation {
         this.gif = gif;
     }
 
-
+/*
     stop() {
         return new Promise((resolve, reject) => {
 
             super.stop().then(() => {
+                this.context = {};
                 this.matrix.clear();
                 this.matrix.render({blend:50});
             })
@@ -34,7 +35,58 @@ module.exports = class GifAnimation extends Animation {
         });
     }    
 
+    start() {
+        return new Promise((resolve, reject) => {
 
+            super.start().then(() => {
+
+                var GIF = require('omggif');
+                var gif = new GIF.GifReader(this.loadGIF(this.gif));
+                var ctx = this.matrix.canvas.getContext('2d');
+                var numFrames = gif.numFrames();
+        
+                var canvas = this.matrix.createCanvas(gif.width, gif.height);
+        
+                var scaleX = this.matrix.width  / gif.width;
+                var scaleY = this.matrix.height / gif.height;
+        
+                ctx.scale(scaleX, scaleY);
+
+                if (scaleX > 1 || scaleY > 1)
+                    ctx.imageSmoothingEnabled = false;                
+
+                this.context = {};
+                this.context.gif = gif;
+                this.context.numFrames = numFrames;
+                this.context.canvas = canvas;
+                this.context.ctx = ctx;
+                this.context.currentFrame = 0;
+            })
+            .then(() => {
+                resolve();
+            })
+            .catch(error => {
+                reject(error);
+            })
+
+        });
+    }  
+
+    render() {
+        var context = this.context;
+
+        var frame = context.gif.frameInfo(i);
+        var image = context.ctx.createImageData(gif.width, gif.height);
+        context.gif.decodeAndBlitFrameRGBA(context.currentFrame, image.data);
+
+        canvas.getContext("2d").putImageData(image, 0, 0);
+        ctx.drawImage(canvas, 0, 0);
+
+        this.matrix.render();
+        this.matrix.sleep(frame.delay * 10);
+
+    }
+*/
     loadGIF(name) {
 
         function fileExists(path) {
